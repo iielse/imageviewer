895436402d6f5a6196dc799012b3311c3ec82d27
wangpeng <whitight@gmail.com>
2025-06-13 17:01:43 +0800

    full dialog

--- app/src/main/java/com/github/iielse/imageviewer/demo/core/viewer/FullScreenImageViewerDialogFragment.kt
+++ app/src/main/java/com/github/iielse/imageviewer/demo/core/viewer/FullScreenImageViewerDialogFragment.kt
@@ -1,18 +1,63 @@
 package com.github.iielse.imageviewer.demo.core.viewer
 
+import android.app.Dialog
+import android.graphics.Color
+import android.os.Build
+import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import android.view.Window
 import android.view.WindowManager
 import com.github.iielse.imageviewer.ImageViewerDialogFragment
+
 
 /**
  * 自定义ImageViewerDialogFragment
  * 此类主要对于 window 进行个性化再定制
  */
 class FullScreenImageViewerDialogFragment : ImageViewerDialogFragment() {
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        return Dialog(requireActivity(), com.github.iielse.imageviewer.demo.R.style.FullScreenDialog).apply {
+            setCanceledOnTouchOutside(true)
+            window?.let(::setWindow)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState).also {
+            // 重要：设置为false，避免系统自动添加padding
+            it?.fitsSystemWindows = false
+        }
+    }
+
+
     override fun setWindow(win: Window) {
-        super.setWindow(win)
-        win.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
-        win.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_FULLSCREEN
+        val layoutParams = WindowManager.LayoutParams()
+        layoutParams.copyFrom(win.attributes)
+        layoutParams.width = WindowManager.LayoutParams.MATCH_PARENT
+        layoutParams.height = WindowManager.LayoutParams.MATCH_PARENT
+
+        // 设置系统UI的可见性，让内容延伸到状态栏和导航栏后面
+        win.attributes = layoutParams
+
+        // 设置系统UI的Flag，使内容可以延伸到状态栏和导航栏区域
+        win.decorView.setSystemUiVisibility(
+            (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION)
+        )
+
+        // 设置状态栏和导航栏透明
+        win.statusBarColor = Color.TRANSPARENT
+        win.navigationBarColor = Color.TRANSPARENT
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            win.attributes.layoutInDisplayCutoutMode = WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES;
+        }
     }
 }
